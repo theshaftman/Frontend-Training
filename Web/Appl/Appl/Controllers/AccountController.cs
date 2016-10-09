b6219e5a81ce7f0a79f24946f3a49416c8549494
theshaftman <mariyan_apostolov@mail.bg>
2016-10-09 21:34:59 +0300

    Login using C# call.

--- Web/Appl/Appl/Controllers/AccountController.cs
+++ Web/Appl/Appl/Controllers/AccountController.cs
@@ -1,10 +1,14 @@
 ﻿using Appl.Models.BusinessLayer;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using System.Web.Security;
 
 namespace Appl.Controllers
@@ -14,38 +18,61 @@ namespace Appl.Controllers
     {
         public ActionResult Login(string returnUrl)
         {
-            ViewBag.GetUserCredentials = "oukASeASeasrlaajkl112112asSaGsaGslaa77612dporqpg1rs12s9937761as3yhmat112aGsasrasrasdaGsASeaGs11212dporasd345xclOIP111.asLKLasS1pl//1112pkalaa12sxcllaahhh098pka12sxcl993GGG";
             ViewBag.ReturnUrl = returnUrl;
 
             return View(false);
         }
-        
+
         // POST: /Account/Login
         [HttpPost]
-        public ActionResult UserLogin(LoginViewModel model, string url)
+        public JsonResult UserLogin(FormCollection model)
         {
-            if (model.Username == null || model.Password == null)
+            if (model["Username"] == null || model["Password"] == null)
             {
-                return Json(new 
+                return Json(new
                 {
                     status = "fail"
                 }, JsonRequestBehavior.AllowGet);
             }
 
-            HttpCookie myCookie = new HttpCookie(Constant.COOKIE_NAME);
-            myCookie["Username"] = model.Username;
-            myCookie.Expires = DateTime.Now.AddDays(5d);
-            Response.Cookies.Add(myCookie);
+            var client = new RestClient("https://baas.kinvey.com/appdata/kid_rJ-gHb40/users/_count?query=%7B%22%24and%22%3A%5B%7B%22username%22%3A%22"
+                + model["Username"].ToString() + "%22%2C%20%22password%22%3A%22" + model["Password"].ToString() + "%22%7D%5D%7D");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("postman-token", "620ae36d-3492-1aad-e4c7-7ea71dd53bdb");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("authorization", "Basic a2lkX3JKLWdIYjQwOmMxNDBmN2UwMDEyZDQ3YjE5YTUzMjc4ZTExYWM1NjRk");
+            IRestResponse response = client.Execute(request);
 
-            return RedirectToAction("Index", "Home");
+            JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+            object isFoundObject = jsonSerializer.DeserializeObject(response.Content);
+            dynamic duff = JsonConvert.DeserializeObject(response.Content);
+            string str = duff["count"].ToString();
+            int number = int.Parse(str);
+
+            bool isFound = number == 1 ? true : false;
+
+            if (isFound)
+            {
+                System.Web.HttpCookie myCookie = new System.Web.HttpCookie(Constant.COOKIE_NAME);
+                myCookie["Username"] = model["Username"].ToString();
+                myCookie.Expires = DateTime.Now.AddDays(5d);
+                Response.Cookies.Add(myCookie);
+            }
+
+            JsonResult result = Json(new
+            {
+                isFound = isFound
+            }, JsonRequestBehavior.AllowGet);
+
+            return result;
         }
-
+        
         [HttpPost]
         public ActionResult UserLogoff()
         {
             if (Request.Cookies[Constant.COOKIE_NAME] != null)
             {
-                HttpCookie myCookie = new HttpCookie(Constant.COOKIE_NAME);
+                System.Web.HttpCookie myCookie = new System.Web.HttpCookie(Constant.COOKIE_NAME);
                 myCookie.Expires = DateTime.Now.AddDays(-1d);
                 Response.Cookies.Add(myCookie);
             }
--- Web/Appl/Appl/Scripts/Login/script.js
+++ Web/Appl/Appl/Scripts/Login/script.js
@@ -15,23 +15,8 @@ $(window).on("load", function () {
 
         queryLogin()
             .done(function (response) {
-                if (response.length == 1) {
-                    $.ajax({
-                        "type": "POST",
-                        "url": urlContent.UrlPath + "/Account/UserLogin",
-                        "data": {
-                            model: {
-                                "Username": currentUserame,
-                                "Password": currentPassword
-                            },
-                            url: "Home"
-                        }
-                    }).done(function (res) {
-                        currentUserame = "";
-                        currentPassword = "";
-
-                        window.location.href = urlContent.UrlPath + "/";
-                    });
+                if (response.isFound) {
+                    window.location.href = urlContent.UrlPath + "/";
                 } else {
                     errorHandle("Error!", "Username or password is not correct!");
                 }
@@ -51,58 +36,18 @@ $(window).on("load", function () {
         currentUserame = $("#username").val();
         currentPassword = $("#password").val();
 
-        var link = unHash(urlContent.GetUserCredentials);
-
-        var obj = $.ajax({
-            "type": "GET",
-            "url": link + "{\"$and\":[{\"username\":\"" + currentUserame + "\", \"password\":\"" + currentPassword + "\"}]}",
-            "headers": {
-                "authorization": "Basic a2lkX3JKLWdIYjQwOmMxNDBmN2UwMDEyZDQ3YjE5YTUzMjc4ZTExYWM1NjRk"
+        var settings = {
+            "type": "POST",
+            "url": urlContent.UrlPath + "/Account/UserLogin",
+            "data": {
+                "Username": currentUserame,
+                "Password": currentPassword
             }
-        });
+        }
+
+        var obj = $.ajax(settings);
 
         return obj;
     }
-
-    function unHash(value) {
-        var hash = ["aGs", "asS", "1as", "asd", "12s", "!as", ".as", "ouk", "por", "pek",
-            "12d", "vbf", "mat", "qpg", "3yh", "asr", "098", "xcl", "laa", "ASe",
-            "pka", "1rs", "9ps", "4p7", "993", "128",
-            "ASW", "QWE", "TTR", "EWE", "AAA", "PRT", "Y6T", "LKL", "MNB", "OIP",
-            "QTY", "VVB", "MNM", "ZZX", "XCX", "CVC", "VBV", "MLP", "NJI", "VGY",
-            "ZSE", "XDR", "CFT", "BHU", "UHB", "TFC",
-            "//1", "154", "165", "098", "1pl", "wer", "100", "--9", "mbo", "]][",
-            "776", "112", "345", "111", "jkl", "hhh", "GGG"];
-        var chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-            ".", "/", "_", "-", ":", "?", "="];
-
-        var parts = value.match(/[\s\S]{1,3}/g) || [];
-        var result = "";
-        var index;
-
-        for (var i = 0; i < parts.length; i++) {
-            index = hash.indexOf(parts[i]);
-
-            if (index == -1) {
-                continue;
-            }
-
-            result += chars[index];
-        }
-
-        return result;
-    }
-
-    //function test(input) {
-    //    var result = "";
-
-    //    for (var i = 0; i < input.length; i++) {
-    //        result += hash[chars.indexOf(input[i])];
-    //    }
-
-    //    return result;
-    //}
-
+    
 });
