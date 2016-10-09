df2ff8c813ca496c8c65dab6c9857519c5210596
theshaftman <mariyan_apostolov@mail.bg>
2016-10-09 12:41:52 +0300

    Update Controller added

---
+++ Web/Appl/GetAppl/Controllers/UpdatesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetAppl.Controllers
+{
+    public class UpdatesController : Controller
+    {
+        // GET: Updates
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
