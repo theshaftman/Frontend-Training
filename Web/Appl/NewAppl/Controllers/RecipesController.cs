cbd24eacabc055d7c1ae742f45dd76aa7d916bd4
theshaftman <mariyan_apostolov@mail.bg>
2017-02-01 20:57:48 +0200

    Side pages added

---
+++ Web/Appl/NewAppl/Controllers/RecipesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewAppl.Controllers
+{
+    public class RecipesController : Controller
+    {
+        // GET: Recipes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
