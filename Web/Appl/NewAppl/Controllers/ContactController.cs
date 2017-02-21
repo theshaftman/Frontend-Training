dc8239587bfa8d8c67d8884852d42c5be189ce9d
theshaftman <mariyan_apostolov@mail.bg>
2017-02-21 21:22:11 +0200

    Send email inclusion

--- Web/Appl/NewAppl/Controllers/ContactController.cs
+++ Web/Appl/NewAppl/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NewAppl.Models.BusinessLayer.Structure;
+using NewAppl.Models.Data;
+using NewAppl.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,29 @@ namespace NewAppl.Controllers
 {
     public class ContactController : Controller
     {
+        IEmailModel _model;
+
+        public ContactController()
+        {
+            this._model = new EmailModel();
+        }
+
         // GET: Contact
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMail(FormCollection model)
+        {
+            Result sendMail = this._model.SendMail(model);
+            JsonResult result = Json(new 
+            {
+                data = sendMail.Data,
+                status = sendMail.Status
+            }, JsonRequestBehavior.AllowGet);
+            return result;
         }
     }
 }
---
+++ Web/Appl/NewAppl/Models/Data/EmailModel.cs
@@ -0,0 +1,62 @@
+﻿using NewAppl.Models.BusinessLayer.Structure;
+using NewAppl.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewAppl.Models.Data
+{
+    internal class EmailModel : IEmailModel
+    {
+        internal EmailModel()
+        {
+
+        }
+
+        Result IEmailModel.SendMail(FormCollection model)
+        {
+            string name = "Mariyan";
+            string subject = "Тестови имейл";
+            string body = "Здравей, изпращам ти тестови имейл :)\nTest";
+
+            var fromAddress = new MailAddress(Constant.FROM_EMAIL, name);
+            var toAddress = new MailAddress(Constant.TO_EMAIL, Constant.TO_NAME);
+            const string fromPassword = Constant.PASSWORD;
+            Result result = new Result();
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                try
+                {
+                    smtp.Send(message);
+                    result.Data = "Successful send!";
+                    result.Status = "success";
+                }
+                catch (Exception e)
+                {
+                    result.Data = "Failure!";
+                    result.Status = "fail";
+                }
+
+                return result;
+            }
+        }
+    }
+}
